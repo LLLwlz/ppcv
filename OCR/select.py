54789494c78ba78a42672026b378104fe5e06956
wlz <1262794380@qq.com>
2022-08-07 11:44:55 +0800

    finished OCR matched

--- OCR/select.py
+++ OCR/select.py
@@ -1,10 +1,11 @@
 from paddleocr import PaddleOCR, draw_ocr
-# 显示结果
 from PIL import Image
 import cv2
 import numpy as np
+
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE' # 修复副本冲突问题
+
 if __name__ == '__main__':
 
     # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
@@ -12,19 +13,8 @@ if __name__ == '__main__':
     ocr = PaddleOCR(use_angle_cls=True, use_gpu=False, lang="ch")  # need to run only once to download and load model into memory
     img_path = 'test.jpg'
     result = ocr.ocr(img_path, cls=True)
+
     for line in result:
+        # print(type(line))
+
         print(line)
-
-
-
-    image = Image.open(img_path).convert('RGB')
-    boxes = [line[0] for line in result]
-    txts = [line[1][0] for line in result]
-    scores = [line[1][1] for line in result]
-    im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/simfang.ttf')
-    im_show = Image.fromarray(im_show)
-    im_show.save('result.jpg')
-    print(type(txts))
-    print(len(txts))
-    print(txts[0])
-    print(boxes[0][1][1])
